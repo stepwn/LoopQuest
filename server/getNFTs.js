06b0214439c06da9c55c22f0ac1855eda250c20e
Stephen <stephen.swanson@live.com>
2023-06-04 13:26:51 -0400

    Delete getNFTs.js

--- server/getNFTs.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-
-axios.get('https://looppress.io/wp-json/looppress/v1/nfts/123')
-  .then(response => {
-    console.log(response.data);
-  })
-  .catch(error => {
-    console.error(error);
-  });
